31156ccf583801aac4056c0b34c3329915453e9c
Olya <106160766+OlyaProkofieva@users.noreply.github.com>
2022-09-22 15:33:49 +0300

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="container-fluid">
         <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <p>
+            Coded by Olha Prokofieva and open-sourced on{" "}
+            <a
+              href="https://github.com/OlyaProkofieva/react-project-weather-app"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              GitHub
+            </a>
+          </p>
+        </footer>
       </div>
     </div>
   );
